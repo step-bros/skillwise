ce4a2872b87db395676f23764ec153a505fd8e01
Lee Jia Keat <leejiakeat@gmail.com>
2023-09-30 11:08:49 +0800

    wat

---
+++ src/main/java/com/stepbros/skillwise/SkillWiseApplication.java
@@ -0,0 +1,13 @@
+package com.stepbros.skillwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepbros/skillwise/SkillWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepbros.skillwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
